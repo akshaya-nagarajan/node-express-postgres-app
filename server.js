a499c595e309878bb266b02d4ac75f70aacae153
akshaya-nagarajan <akshaya.s.nagarajan@gmail.com>
2020-06-04 02:00:11 -0700

    Correct changes

--- server.js
+++ server.js
@@ -9,54 +9,47 @@ const client = new Client({
     port: 5432,
     database: "testinstancedb"
 })
-execute_connection()
+
 async function execute_connection() {
-    try{
-        //await client.connect()
-        console.log("Connected successfully.")
-        
-    }
-    catch (ex)
-    {
-        console.log(`Something wrong happend ${ex}`)
-    }
-    finally 
-    {
-        //await client.end()
-        //console.log("Client disconnected successfully.")    
-    } 
+    await client.connect()
+    console.log("Connected successfully.")
 }
 
+async function execute_connection_close() {
+    await client.end() 
+}
 
-app.get('/users', async (request, response) => {
-    await client.connect()
-    const {rows} = await client.query("select * from \"User\" ")
-    console.table(rows)
-    //response.send({"rows": rows})
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-    await client.end()
-  })
+async function execute_task() {
+    const {rows} = await client.query("select * from \"Task\"  ")
+    return rows;
+}
+async function execute_project() {
+    const {rows} = await client.query("select * from \"Project\"  ")
+    return rows;
+}
+async function execute_user() {
+    const {rows} = await client.query("select * from \"User\"  ")
+    return rows;
+}
 
+execute_connection()
 app.get('/tasks', async (request, response) => {
-    await client.connect()
-    const {task_rows} = await client.query("select * from \"Task\"")
-    client.end()
-    console.table(task_rows)
-    //response.send({"rows": task_rows})
-    //response.json({ info: 'Node.js, Express, and Postgres API' })
-    response.end("something");
-    
+    rows=await execute_task()
+    console.table(rows)
+    response.send({"rows": rows})
 })
 
 app.get('/projects', async (request, response) => {
-    await client.connect()
-    const {project_rows} = await client.query("select * from \"Project\"")
-    console.table(project_rows)
+    rows=await execute_project()
+    console.table(rows)
     response.send({"rows": rows})
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-    await client.end()
 })
 
+app.get('/users', async (request, response) => {
+    rows=await execute_user()
+    console.table(rows)
+    response.send({"rows": rows})
+})
 
 execute_express()
 async function execute_express() {
